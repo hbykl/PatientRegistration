16857ba7f6b02ea7674b4c53cfc5d8e721c8edcb
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-08-23 20:41:20 +0300

    Patiens Model

---
+++ src/main/java/com/example/PatientRegistration/domain/model/Patiens.java
@@ -0,0 +1,58 @@
+package com.example.PatientRegistration.domain.model;
+
+public class Patiens {
+    String name;
+    String surname;
+    int age;
+    String phone;
+    String email;
+
+    public Patiens(String name, String surname, int age, String phone, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/com/example/PatientRegistration/web/controller/PatientController.java
@@ -0,0 +1,5 @@
+package com.example.PatientRegistration.web.controller;
+
+public class PatientController {
+
+}
