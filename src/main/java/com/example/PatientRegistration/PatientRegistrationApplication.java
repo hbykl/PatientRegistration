44f9ec207557c3069635a259a2edcf6efb1e49ee
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-08-23 19:05:37 +0300

    Initial commit

---
+++ src/main/java/com/example/PatientRegistration/PatientRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.PatientRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRegistrationApplication.class, args);
+	}
+
+}
