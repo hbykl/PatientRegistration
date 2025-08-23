dcedcfa26ed266d4f8ecb4bf4c4d3c20d4983ef5
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-08-23 22:56:22 +0300

    Update PatientsService.java

--- src/main/java/com/example/PatientRegistration/application/PatientsService.java
+++ src/main/java/com/example/PatientRegistration/application/PatientsService.java
@@ -1,24 +1,60 @@
 package com.example.PatientRegistration.application;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 import com.example.PatientRegistration.domain.model.Patients;
 
 public class PatientsService {
-    HashMap<String, String> patientsMap = new HashMap<String, String>();
-    FileService fileService = new FileService();
-    PatientJsonCodec patientJsonCodec;
+    private HashMap<String, Patients> patientsMap = new HashMap<String, Patients>();
 
-    public void createPatient(String fileName, String name, String surname, int age, String phone, String email) {
+    public void loadPatientsFromFile(String filename) {
+        FileService fileService = new FileService();
+        List<String> lines = fileService.readAllLinesFromFile(filename);
+        for (String line : lines) {
+            try {
+                Patients patient = PatientJsonCodec.fromNdjson(line);
+                patientsMap.put(patient.getId(), patient);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void createPatient(String name, String surname, int age, String phone, String email) {
         UUID uuid = UUID.randomUUID();
         String uuidAsString = uuid.toString();
         Patients patient = new Patients(uuidAsString, name, surname, age, phone, email);
-        patientsMap.put(uuidAsString, PatientJsonCodec.toNdjson(patient));
+        patientsMap.put(uuidAsString, patient);
     }
 
-    public HashMap<String, String> getPatiens() {
-        return patientsMap;
+    public void deletePatient(String id) {
+        if (!patientsMap.containsKey(id)) {
+            System.out.println("Verilen değere ait hasta bulunamadı");
+            return;
+        }
+        patientsMap.remove(id);
+    }
+
+    public List<Patients> getAllPatients() {
+        return new ArrayList<>(patientsMap.values());
+    }
+
+    public void updatePatient(Patients patient,
+            String name,
+            String surname,
+            int age,
+            String phone,
+            String email) {
+        if (!patientsMap.containsKey(patient.getId())) {
+            System.out.println("Verilen değere ait hasta bulunamadı");
+            return;
+        }
+        Patients updatedPatient = new Patients(patient.getId(), name, surname, age, phone, email);
+        patientsMap.put(patient.getId(), updatedPatient);
+
     }
 
 }
